a92c3cb2c59b601b61ac0ead37e1bb677fe61331
8ohamed <114395272+8ohamed@users.noreply.github.com>
2022-11-03 13:22:18 +0100

    Update sum.c

--- src/sum.c
+++ src/sum.c
@@ -2,5 +2,10 @@
 
 int sum(int a[], int n)
 {
+  if (n>0) // hvis den ikker er større end 0 så skal den retuner 0 
+    return a[n-1]+sum(a,n-1); //sum af den nuværende tal og den næste i arrayet
+
+    else
     return 0;
+
 }
