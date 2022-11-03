778dcc10827ff94c0229fbf76d58145c1af0a77a
8ohamed <114395272+8ohamed@users.noreply.github.com>
2022-11-03 13:22:44 +0100

    Update search.c

--- src/search.c
+++ src/search.c
@@ -3,5 +3,13 @@
 
 bool search(int a[], int n, int x)
 {
+  if (n>0 && a[n-1]!=x) //hvis den er er større end 0 og den fundet tal er ikke = x så skal den blive ved 
+    return search (a,n-1,x);
+
+else if 
+    (n>0&&a[n-1]==x)   // hvis den er større end 0 og den fundet tal er = x så retuner den 1 dvs. true
+    return 1;
+
+else // ellers retuner 0 dvs. false
     return 0;
 }
